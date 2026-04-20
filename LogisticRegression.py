34b1582c2f7b9b4bcba94ef3d01884e145148a91
Hemant Rathore <hemant0hack@gmail.com>
2026-04-20 23:30:55 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -2,17 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 
-X = np.array([[1], [2], [3], [4], [5], [6], [7], [8]])
-y = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+# Input: study hours
+X = np.array([1, 2, 3, 4, 5, 6]).reshape(-1, 1)
 
+# Output: 0 = fail, 1 = pass
+y = np.array([0, 0, 0, 1, 1, 1])
+
+# Create model
 model = LogisticRegression()
 
+# Train model
 model.fit(X, y)
 
-hours = [[4.5]]
-prediction = model.predict(hours)
+# Predict for 5 hours
+prediction = model.predict([[5]])
+probability = model.predict_proba([[5]])
 
-print("Prediction (0 = Fail, 1 = Pass):", prediction)
+print("Prediction:", prediction[0])
+print("Probability:", probability)
 
 plt.scatter(X, y, color='blue')
 plt.xlabel("Study Hours")
